7fc7377c364393a042f2bb22a9acfa46da5e3a80
Fazil <fazilm@gmail.com>
2017-04-19 09:26:50 -0700

    return post response

--- polls/views.py
+++ polls/views.py
@@ -3,4 +3,10 @@ from django.http import HttpResponse
 
 def index(request):
     from django.http import JsonResponse
-    return JsonResponse({'blah':'polls-hahaha'})
+    import json
+    my_json = json.loads(request.body)
+    response_dict = {}
+    for key in my_json:
+        response_dict['key'] = key
+        response_dict['value'] = my_json[key]
+    return JsonResponse(response_dict)
