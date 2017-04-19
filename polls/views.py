b594a7a953df1b7de2d143fed69929e0beaecc80
Fazil <fazilm@gmail.com>
2017-04-19 16:00:48 -0700

    VGS is wokring now?!!?

--- polls/views.py
+++ polls/views.py
@@ -8,14 +8,13 @@ def index(request):
     body_unicode = request.body.decode('utf-8')
     # my_json = json.loads(body_unicode)
 
-    # response_dict = {}
-    # for key in my_json:
-    #     response_dict['key'] = key
-    #     response_dict['value'] = my_json[key]
-    # return JsonResponse(response_dict)
-    from urllib import parse
-    parse.unquote(body_unicode)
     response_dict = {}
-    # response_dict["body"] = request.body
-    response_dict['decoded_body'] = body_unicode
+    for key in my_json:
+        response_dict['key'] = key
+        response_dict['value'] = my_json[key]
     return JsonResponse(response_dict)
+    # from urllib import parse
+    # response_dict = {}
+    # # response_dict["body"] = request.body
+    # response_dict['decoded_body'] = body_unicode
+    # return JsonResponse(response_dict)
