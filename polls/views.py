b65d9019e045a9efc2e09e62ebf142e41d9d1942
Fazil <fazilm@gmail.com>
2017-04-19 15:45:07 -0700

    Print the request body in the json response for debugging

--- polls/views.py
+++ polls/views.py
@@ -6,10 +6,14 @@ def index(request):
     import json
     # my_json = json.loads(request.body)
     body_unicode = request.body.decode('utf-8')
-    my_json = json.loads(body_unicode)
+    # my_json = json.loads(body_unicode)
+
+    # response_dict = {}
+    # for key in my_json:
+    #     response_dict['key'] = key
+    #     response_dict['value'] = my_json[key]
+    # return JsonResponse(response_dict)
 
     response_dict = {}
-    for key in my_json:
-        response_dict['key'] = key
-        response_dict['value'] = my_json[key]
+    response_dict['body'] = body_unicode
     return JsonResponse(response_dict)
