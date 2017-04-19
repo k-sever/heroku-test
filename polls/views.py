9b4c97441059308a10023f2cf1f80c09f25dff15
Fazil <fazilm@gmail.com>
2017-04-19 15:26:31 -0700

    decode request body before parsing json

--- polls/views.py
+++ polls/views.py
@@ -4,7 +4,10 @@ from django.http import HttpResponse
 def index(request):
     from django.http import JsonResponse
     import json
-    my_json = json.loads(request.body)
+    # my_json = json.loads(request.body)
+    body_unicode = request.body.decode('utf-8')
+    my_json = json.loads(body_unicode)
+
     response_dict = {}
     for key in my_json:
         response_dict['key'] = key
