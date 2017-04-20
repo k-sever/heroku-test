db3f50a26ccda272fb6f8ae4271e9efa3a477bff
Fazil <fazilm@gmail.com>
2017-04-20 16:29:08 -0700

    Removing unused code before sending to VGS

--- polls/urls.py
+++ polls/urls.py
@@ -4,6 +4,5 @@ from django.views.decorators.csrf import csrf_exempt
 from . import views
 
 urlpatterns = [
-    # url(r'^post$', csrf_exempt(views.index), name='index'),
     url(r'^post$', csrf_exempt(views.test), name='test'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -3,24 +3,6 @@ from django.shortcuts import render
 
 from .models import Name
 
-def index(request):
-    from django.http import JsonResponse
-    import json
-    # my_json = json.loads(request.body)
-    body_unicode = request.body.decode('utf-8')
-    my_json = json.loads(body_unicode)
-
-    response_dict = {}
-    for key in my_json:
-        response_dict['key'] = key
-        response_dict['value'] = my_json[key]
-    return JsonResponse(response_dict)
-    # from urllib import parse
-    # response_dict = {}
-    # # response_dict["body"] = request.body
-    # response_dict['decoded_body'] = body_unicode
-    # return JsonResponse(response_dict)
-
 def test(request):
     if request.method == 'POST':
         return render(request, "polls/test.html", {'name': request.POST['your_name']})
