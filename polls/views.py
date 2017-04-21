7659ef23d014c0094709cd3d6afc5b762fdbc122
Fazil <fazilm@gmail.com>
2017-04-21 13:35:31 -0700

    Adding second way of submitting forms using ajax

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+import json
 from .models import Name
 
 def test(request):
     if request.method == 'POST':
-        return render(request, "polls/test.html", {'name': request.POST['your_name']})
+        return HttpResponse(json.dumps({'name': request.POST['your_name']}), content_type="application/json")
+        # return render(request, "polls/test.html", {'name': request.POST['your_name']})
     return render(request, "polls/test.html")
