0c178fddd2e70aae89d1e068f0a220871f1c7c0e
Fazil <fazilm@gmail.com>
2017-04-24 16:37:05 -0700

    Set csrf token in ajax header

--- polls/models.py
+++ polls/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 # Create your models here.
 class Name(models.Model):
-    name = models.CharField(max_length=500, unique=True)
+    name = models.CharField(max_length=250, unique=True)
